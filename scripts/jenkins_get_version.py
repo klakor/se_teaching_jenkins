bfce9293bcb415b41986d2faa196f3afa0d0e387
klakor <klaudia.korusiewicz@gmail.com>
2020-05-24 12:26:16 +0200

    jenkins_get_version.py modified

--- scripts/jenkins_get_version.py
+++ scripts/jenkins_get_version.py
@@ -7,4 +7,7 @@ server = jenkins.Jenkins('http://localhost:8080',
 
 user = server.get_whoami()
 version = server.get_version()
+plugins = server.get_plugins_info()
+
 print('Hello %s from Jenkins %s' % (user['fullName'], version))
+print ('Installed plugins: ', plugins)
